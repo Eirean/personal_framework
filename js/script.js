519be40e99a9c83e1da7145b1eb8140862e2d882
Eirean <patyjewi@hawaii.edu>
2023-10-17 13:06:00 -1000

    added toggle button

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ const myobserver = new IntersectionObserver((entries) => {
 document.querySelectorAll(".animate-me-on-scroll").forEach((section) => {
     myobserver.observe(section);
 });
+
+// TOGGLE THE MAIN MENU VIA MENU BUTTON
+const mymenubutton = document.querySelector('.site-header .menu-button');
+const mysitenav = document.querySelector('.site-header .site-nav');
+
+mymenubutton.onclick = function() {
+    // if nav is open, then close it; else open it
+    if ( mysitenav.getAttribute('data-navstate') === 'open') {
+        mysitenav.setAttribute( 'data-navstate','closed');
+    } else {
+        mysitenav.setAttribute('data-navstate','open');
+    }
+};
